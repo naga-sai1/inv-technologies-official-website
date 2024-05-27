76ccd269ace1384d998af05b07b06f403cb12ac3
Durga Potnuru <vijaypotnuru123@gmail.com>
2024-05-27 17:34:30 +0530

    first commit

---
+++ scss/config.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDOMD2QJ9USmCs6EHgqnFUPbkhiwr82l2Q",
+  authDomain: "pbg-project.firebaseapp.com",
+  projectId: "pbg-project",
+  storageBucket: "pbg-project.appspot.com",
+  messagingSenderId: "916381281040",
+  appId: "1:916381281040:web:1b19f903e270bc1928d395",
+  measurementId: "G-43CP62V2C1",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
